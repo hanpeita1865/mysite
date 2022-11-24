688133255c61bd24e15b974c16e3889bce04c623
hanpeita1865 <93022322+hanpeita1865@users.noreply.github.com>
2022-11-24 09:15:07 +0900

    vue_cli_sample

--- p__js_XMLHttpRequest/sample/sample1/script.js
+++
@@ -1,7 +0,0 @@
-let ajax = new XMLHttpRequest();
- 
-ajax.open("get", "files/test.txt");
-ajax.send(); // 通信させます。
-ajax.addEventListener("load", function(){ // loadイベントを登録します。
-	console.log(this.response); // 通信結果を出力します。
-}, false);
--- p__js_XMLHttpRequest/sample/sample2/script.js
+++
@@ -1,9 +0,0 @@
-let ajax = new XMLHttpRequest();
- 
-ajax.open("get", "files/test.txt");
-ajax.setRequestHeader("Content-Type", "application/json;charset=UTF-8"); // JSONとしてヘッダーを指定。
-ajax.setRequestHeader("Cache-Control", "no-cache"); //キャッシュの無効化
-ajax.send(JSON.stringify('[0, 1, 2]')); // 通信させます。
-ajax.addEventListener("load", function(){ // loadイベントを登録します。
-	console.log(this.response); // 通信結果を出力します。
-}, false);
